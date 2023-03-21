714c1f53453c74d9893fb3db7bae50d85171f79c
johndesk2205 <joaomarcos2205@gmail.com>
2023-03-21 16:58:32 -0300

    Resolução do exercícios

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const minhaStringNoSpace = minhaString.trim()
+
+console.log(minhaStringNoSpace)
+
+console.log("A string com os espaços:", minhaString.length)
+
+console.log("A string sem os espaços:", minhaStringNoSpace.length)
+
+console.log(minhaStringNoSpace.replace("________", "sticioso"))
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,30 @@
-//Crie a const para a frase aqui
+// a) Crie uma const no seu código para guardar a frase (com aspas e tudo);
+
+// b) Crie uma nova string a partir da primeira, trocando verde por rosa, e azul por laranja;
+
+// c) Verifique se a nova string inclui verde, e se inclui laranja.
+
+// EXTRA: tente fazer o trecho “mas não deixe o gato sair” ficar em letras maiúsculas, assim como o “BOAS VINDAS”
+//===========================================================================
+
+// a) Crie uma const no seu código para guardar a frase (com aspas e tudo);
+const string = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+
+console.log(string)
+
+// b) Crie uma nova string a partir da primeira, trocando verde por rosa, e azul por laranja;
+const newString = string.replace("verde", "rosa").replace("azul", "laranja")
+
+console.log(newString)
+
+// c) Verifique se a nova string inclui verde, e se inclui laranja.
+console.log("Possui a palavra verde?", newString.includes("verde"))
+console.log("Possui a palavra laranja?", newString.includes("laranja"))
+
+// EXTRA: tente fazer o trecho “mas não deixe o gato sair” ficar em letras maiúsculas, assim como o “BOAS VINDAS”
+
+const newStringUpperCase = newString.replace("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR")
+
+console.log(newStringUpperCase)
+
+
